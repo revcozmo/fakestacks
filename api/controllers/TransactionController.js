59eadbafcb645a25acce5dcab00701fe03b0cc70
Jason Goetz <jgoetz@jamasoftware.com>
2015-11-21 23:09:51 -0800

    no message

--- api/controllers/TransactionController.js
+++ api/controllers/TransactionController.js
@@ -8,7 +8,7 @@
 module.exports = {
 	show: function(req, res, next) {
 		var userId = req.param('id');
-		Bet.find().where({user: userId ? userId : req.session.User.id}).populate('bettable').populate('user').exec(function foundBets(err, bets) {
+		Bet.find().where({user: userId ? userId : req.session.User.id}).sort({ time: 'asc' }).populate('bettable').populate('user').exec(function foundBets(err, bets) {
 			if (err) return next(err);
 			if (!bets) return next();
 			var runningTally = sails.config.league.startingAccount;
