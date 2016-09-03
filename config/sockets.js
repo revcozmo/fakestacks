d72d30163dc657e8ff211601c59f99d295778fc3
Jason Goetz <jgoetz@jamasoftware.com>
2016-09-02 17:10:40 -0700

    Turning off redis socket again. It broke the app

--- config/sockets.js
+++ config/sockets.js
@@ -39,16 +39,16 @@ module.exports.sockets = {
   *                                                                          *
   ***************************************************************************/
 
-  transports: [
-    'websocket'
-  ],
-
-  adapter: 'socket.io-redis',
-
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  db: process.env.REDIS_DB,
-  pass: process.env.REDIS_PASS,
+  // transports: [
+  //   'websocket'
+  // ],
+  //
+  // adapter: 'socket.io-redis',
+  //
+  // host: process.env.REDIS_HOST,
+  // port: process.env.REDIS_PORT,
+  // db: process.env.REDIS_DB,
+  // pass: process.env.REDIS_PASS,
 
 
   // adapter: 'memory',
